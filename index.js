bb21e7534aed465770d7098becf82e7bf5ad6121
williamsears <wpeter1@hotmail.com>
2015-12-02 16:56:10 +0000

    added search exercise 5

--- index.js
+++ index.js
@@ -1,20 +1,63 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+
+var entries = {
+  1: {
+    firstName: "John",
+    lastName: "Smith",
+    emails: [
+      {type: "home", address: "john@smith.com"},
+      {type: "work", address: "jsmith@megacorp.com"}
+    ]
+  }, 
+  2: {
+    firstName: "JOHN",
+    lastName: "CENA",
+    emails: [
+      {type: "home", address: "JOHNCENA@WWE.com"},
+      {type: "work", address: "JOHNCENA@WWE.com"}
+    ]
+  },
+};
+
+app.get('/entry/search', function(req, res){
+    var matches = [];
+    
+    if(req.query.firstName){
+        for(var i in entries){
+            if((entries[i].firstName).toLowerCase() === (req.query.firstName).toLowerCase() || (entries[i].lastName).toLowerCase() === (req.query.lastName).toLowerCase()){
+                matches.push(entries[i]);
+            }
+        }
+    } else if (req.query.email){
+        for(var i in entries){
+            entries[i].emails.forEach(function(email){
+                if(email.address.indexOf(req.query.email) > -1){
+                    res.json(entries[i])
+                }
+            })
+        }
+    } else {
+        res.status(404).send();
+    }
+    
+    res.json(matches);
+    
+    
+    
+})
 
 
 
 
 
-/* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
-// Boilerplate code to start up the web server
-var server = app.listen(process.env.PORT, process.env.IP, function () {
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+
+
+
+
+app.listen(process.env.PORT, function(){
+  console.log("Server Listening")
+})
