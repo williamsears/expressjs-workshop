afcf62913de09981c4765ba9e730c8f4929e94e6
williamsears <wpeter1@hotmail.com>
2015-12-02 16:10:39 +0000

    added entry server exercise4

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var entries = {
+  1: {
+    firstName: "John",
+    lastName: "Smith",
+    emails: [
+      {type: "home", address: "john@smith.com"},
+      {type: "work", address: "jsmith@megacorp.com"}
+    ]
+  },
+    2: {
+    firstName: "John",
+    lastName: "AMERICA",
+    emails: [
+      {type: "home", address: "john@smith.com"},
+      {type: "work", address: "jsmith@megacorp.com"}
+    ]
+  },
+    3: {
+    firstName: "John",
+    lastName: "Appleseed",
+    emails: [
+      {type: "home", address: "john@smith.com"},
+      {type: "work", address: "jsmith@megacorp.com"}
+    ]
+  },
+    4: {
+    firstName: "John",
+    lastName: "CENA",
+    emails: [
+      {type: "home", address: "john@smith.com"},
+      {type: "work", address: "jsmith@megacorp.com"}
+    ]
+  }
+  
+};
+
+app.get('/entry/:entryId', function (req, res) {
+  var current =  req.params.entryId;
+  res.send(entries[current]);
 });
 
 
